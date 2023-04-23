543146c7896e9491dc90c30b2a2edfc9bbc43030
ciborg228 <voigva@yandex.ru>
2023-04-23 19:57:01 +0300

    полностью изменил код

--- bot.py
+++ bot.py
@@ -1,133 +1,100 @@
-import datetime as dt
-import logging
+import telebot
+import sqlite3
+from telebot import types
+from config import bot
 
-from telegram.ext import Application, MessageHandler, filters
-# Добавим необходимый объект из модуля telegram.ext
-from telegram.ext import CommandHandler
-
-from config import BOT_TOKEN
-
-TIMER = 5
-# Запускаем логгирование
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
-)
-
-logger = logging.getLogger(__name__)
+text = []
 
 
-def remove_job_if_exists(name, context):
-    """Удаляем задачу по имени.
-    Возвращаем True если задача была успешно удалена."""
-    current_jobs = context.job_queue.get_jobs_by_name(name)
-    if not current_jobs:
-        return False
-    for job in current_jobs:
-        job.schedule_removal()
-    return True
+@bot.message_handler(commands=['dobavlenie'])
+def application(message):
+    rmk = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    rmk.add(types.KeyboardButton("Да"), types.KeyboardButton("Нет"))
+
+    msg = bot.send_message(message.chat.id, "Желаете зарегестрироваться?",
+                           reply_markup=rmk)
+    bot.register_next_step_handler(msg, user_answer)
 
 
-# Определяем функцию-обработчик сообщений.
-# У неё два параметра, updater, принявший сообщение и контекст - дополнительная информация о сообщении.
-async def echo(update, context):
-    # У объекта класса Updater есть поле message,
-    # являющееся объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str),
-    # отсылающий ответ пользователю, от которого получено сообщение.
-    await update.message.reply_text(f'ваааау, у меня прям такая же')
+def user_answer(message):
+    if message.text == "Да":
+        msg = bot.send_message(message.chat.id, "Впишите ваши данные")
+        bot.register_next_step_handler(msg, user_reg)
+    elif message.text == "нет":
+        bot.send_message(message.chat.id, "Без регистрации вы не сможете добавить свою историю")
+    else:
+        bot.send_message(message.chat.id, "Я что-то не понимаю вашей речи")
 
 
-# Напишем соответствующие функции.
-# Их сигнатура и поведение аналогичны обработчикам текстовых сообщений.
-async def start(update, context):
-    await update.message.reply_text(
-        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
-        "Вы можете прервать опрос, послав команду /skip.\n"
-        "В каком городе вы живёте?")
+def user_reg(message):
+    bot.send_message(message.chat.id, f"Your data: {message.text}")
+    pmk = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    pmk.add(types.KeyboardButton("Да"), types.KeyboardButton("Нет"))
 
-    # Число-ключ в словаре states —
-    # втором параметре ConversationHandler'а.
-    return 1
-    # Оно указывает, что дальше на сообщения от этого пользователя
-    # должен отвечать обработчик states[1].
-    # До этого момента обработчиков текстовых сообщений
-    # для этого пользователя не существовало,
-    # поэтому текстовые сообщения игнорировались.
+    mpg = bot.send_message(message.chat.id, "Желаете добавить свою историю?",
+                           reply_markup=pmk)
+    bot.register_next_step_handler(mpg, user_history)
 
 
-async def first_response(update, context):
-    # Это ответ на первый вопрос.
-    # Мы можем использовать его во втором вопросе.
-    locality = update.message.text
-    await update.message.reply_text(
-        f"Какая погода в городе {locality}?")
-    # Следующее текстовое сообщение будет обработано
-    # обработчиком states[2]
-    return 2
+def user_history(message):
+    if message.text == "Да":
+        mpg = bot.send_message(message.chat.id, "Впишите вашу историю")
+        bot.register_next_step_handler(mpg, user_dobav)
+    elif message.text == "нет":
+        bot.send_message(message.chat.id, "{Хорошо, надеюсь вы найдёте время написать её")
+    else:
+        bot.send_message(message.chat.id, "Это точно история?")
 
 
-async def second_response(update, context):
-    # Ответ на второй вопрос.
-    # Мы можем его сохранить в базе данных или переслать куда-либо.
-    weather = update.message.text
-    logger.info(weather)
-    await update.message.reply_text("Спасибо за участие в опросе! Всего доброго!")
-    return ConversationHandler.END  # Константа, означающая конец диалога.
-    # Все обработчики из states и fallbacks становятся неактивными.
+def user_dobav(message):
+    bot.send_message(message.chat.id, "ваша история, успешно добавлена(нет)")
+    text.append(message.text)
+    print(message.text)
 
 
-async def skip(update, context):
-    await update.message.reply_text("«Какая погода у вас за окном?»")
-    return ConversationHandler.END
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, " Здравствуйте, приветствую Вас здесь. "
+                          "Бот пока что создаётся, и его основные команды: "
+                          "/info и /dobavlenie. ")
 
 
-async def help_command(update, context):
-    """Отправляет сообщение когда получена команда /help"""
-    await update.message.reply_text("мне бы кто помог...")
+@bot.message_handler(commands=['info'])
+def get_user_info(message):
+    markup_inline = types.InlineKeyboardMarkup()
+    item_yes = types.InlineKeyboardButton(text='ДА', callback_data='yes')
+    item_no = types.InlineKeyboardButton(text='НЕТ', callback_data='no')
+
+    markup_inline.add(item_yes, item_no)
+    bot.send_message(message.chat.id, 'Желаете узнать небольшую информацию о вас?',
+                     reply_markup=markup_inline
+                     )
 
 
-async def get_time(update, context):
-    """Отправляет сообщение когда получена команда /help"""
-    await update.message.reply_text(str(dt.datetime.now().time()))
+@bot.callback_query_handler(func=lambda call: True)
+def answer(call):
+    if call.data == 'yes':
+        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item_id = types.KeyboardButton("МОЙ ID")
+        item_username = types.KeyboardButton("МОЙ НИК")
+
+        markup_reply.add(item_id, item_username)
+        bot.send_message(call.message.chat.id, "Нажмите на то, что вы бы хотели узнать",
+                         reply_markup=markup_reply
+                         )
+    elif call.data == 'no':
+        bot.send_message(call.message.chat.id, "ну и иди далеко, и на долго")
+
+# следующая функция показ айди и ника
+# Показываетс он средством двух кнопок, которые находятся внизу панели
+# конпка: МОЙ ID, показывает ваш ID, вауууу
+#кнопка: МОЙ НИК, показывает вашь ник
+@bot.message_handler(content_types=['text'])
+def get_text(message):
+    if message.text == "МОЙ ID":
+        bot.send_message(message.chat.id, f"ваш айди: {message.from_user.id}")
+    elif message.text == 'МОЙ НИК':
+        bot.send_message(message.chat.id, f"ваш ник: {message.from_user.first_name} {message.from_user.last_name}")
 
 
-async def get_data(update, context):
-    """Отправляет сообщение когда получена команда /help"""
-    await update.message.reply_text(str(dt.datetime.now().date()))
-
-
-
-
-def main():
-    # Создаём объект Application.
-    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
-    application = Application.builder().token(BOT_TOKEN).build()
-
-    # Создаём обработчик сообщений типа filters.TEXT
-    # из описанной выше асинхронной функции echo()
-    # После регистрации обработчика в приложении
-    # эта асинхронная функция будет вызываться при получении сообщения
-    # с типом "текст", т. е. текстовых сообщений.
-    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("time", get_time))
-    application.add_handler(CommandHandler("date", get_data))
-    application.add_handler(CommandHandler("unset", unset))
-    application.add_handler(CommandHandler("skip", skip))
-
-    # Регистрируем обработчик в приложении.
-    application.add_handler(text_handler)
-
-    # Запускаем приложение.
-    application.run_polling()
-    # Зарегистрируем их в приложении перед
-    # регистрацией обработчика текстовых сообщений.
-    # Первым параметром конструктора CommandHandler я
-    # вляется название команды.
-
-
-# Запускаем функцию main() в случае запуска скрипта.
-if __name__ == '__main__':
-    main()
+bot.polling()
