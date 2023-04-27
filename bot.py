dfdfb271dcedbe0f8f113961badc7e1a51df4f14
Yandex05 <voigva@yandex.ru>
2023-04-27 16:57:10 +0300

    начальный код, либо всё сломается, либо будет чикипуки добавил config перевёл всё на другую часть переделка кода ещё одна небольшая модификация и ещё парочка и ещё elfkbk config финальная форма

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 import telebot
 from telebot import types
 
@@ -5,7 +7,6 @@ from config import TOKEN
 
 text = []
 bot = telebot.TeleBot(TOKEN)
-
 
 
 @bot.message_handler(commands=['dobavlenie'])
@@ -23,12 +24,12 @@ def application(message):
     bot.register_next_step_handler(msg, user_answer)
 
 
-# если вы ответили да
-# поздравляю, впишите логин или пароль
-# если нет, то он может вас предупредить
-# так же бот может вас не понять
-# но дл этого надо постараться
 def user_answer(message):
+    '''если вы ответили да
+поздравляю, впишите логин или пароль
+если нет, то он может вас предупредить
+так же бот может вас не понять
+но дл этого надо постараться'''
     if message.text == "Да":
         msg = bot.send_message(message.chat.id, "Впишите ваши данные")
         bot.register_next_step_handler(msg, user_reg)
@@ -49,9 +50,9 @@ def user_reg(message):
     bot.register_next_step_handler(mpg, user_history)
 
 
-# тут вы уже можете вписать свою историю
-# если же у вас нет времени, то бот просто предложить написать в другой раз
 def user_history(message):
+    '''тут вы уже можете вписать свою историю
+если же у вас нет времени, то бот просто предложить написать в другой раз'''
     if message.text == "Да":
         mpg = bot.send_message(message.chat.id, "Впишите вашу историю")
         bot.register_next_step_handler(mpg, user_dobav)
@@ -64,72 +65,52 @@ def user_history(message):
 def user_dobav(message):
     bot.send_message(message.chat.id, "ваша история, успешно добавлена(нет)")
     text.append(message.text)
-    print(message.text)
+    name = message.from_user.id
+    print(name, '==', message.text)
+    f = open(f"histori/{name}.txt", 'a', encoding='UTF8')
+    print(f'{dt.datetime.now()}, {message.text}\n', file=f)
+    f.close()
 
 
-# приветствие и ознакомление с ботом
 @bot.message_handler(commands=['start'])
 def start(message):
+    '''приветствие и ознакомление с ботом'''
     bot.reply_to(message, " Здравствуйте, приветствую Вас здесь. "
                           "Бот пока что создаётся, и его основные команды: "
                           "/info и /dobavlenie. "
                           "ну и /help, для удобства")
+    idphoto = open('1550472288_1.jpg', 'rb')
+    bot.send_photo(message.chat.id, photo=idphoto)
 
 
 @bot.message_handler(commands=['help'])
 def start(message):
-    bot.reply_to(message, "перечень доступных команд:"
-                          "/picha ")
+    bot.reply_to(message, "нужна помощь?\n"
+                          "я вам не помощник")
 
 
-# обавлено по приколу
-# должно вывести несколько вопросов
-@bot.message_handler(commands=["picha"])
-def inline(message):
-    key = types.InlineKeyboardMarkup()
-    but_1 = types.InlineKeyboardButton(text="Где проходит", callback_data="Где проходит")
-    but_2 = types.InlineKeyboardButton(text="Кто спикеры", callback_data="Кто спикеры")
-    but_22 = types.InlineKeyboardButton(text="Сколько стоит", callback_data="Сколько стоит")
-    key.add(but_1, but_2, but_22)
-    bot.send_message(message.chat.id,
-                     "Рады приветствовать Вас! В преддверии главного праздника весны мы организуем самое масштабное событие для ярких, стильных и успешных женщин!")
-    bot.send_message(message.chat.id, "Картинка", reply_markup=key)
-
-
-@bot.callback_query_handler(func=lambda c: True)
-def inlin(c):
-    if c.data == "Где проходит":
-        bot.send_photo(c.chat_id, photo=("Photo.PNG"))
-    elif c.data == "Кто спикеры":
-        bot.send_message(c.message.chat.id, "Ответ спикеры")
-    elif c.data == "Сколько стоит":
-        bot.send_message(c.message.chat.id, "стоимость")
-
-
-# здесь задаётся основной вопрос
-# на него можете ответить либо да
-# либо нет
 @bot.message_handler(commands=['info'])
 def get_user_info(message):
+    '''здесь задаётся основной вопрос
+ на него можете ответить либо да либо нет'''
     markup_inline = types.InlineKeyboardMarkup()
     item_yes = types.InlineKeyboardButton(text='ДА', callback_data='yes')
     item_no = types.InlineKeyboardButton(text='НЕТ', callback_data='no')
 
     markup_inline.add(item_yes, item_no)
     bot.send_message(message.chat.id, 'Желаете узнать небольшую информацию о вас?',
-                     reply_markup=markup_inline
-                     )
+                     reply_markup=markup_inline)
 
 
-# следующая функция показ айди и ника
-# он задаёт вопрос, на который вы можете ответить да, или нет
-# при согласии бот даёт выбор:
-# Показывает две кнопки, которые находятся внизу панели
-# конпка: МОЙ ID, показывает ваш ID, вауууу
-# кнопка: МОЙ НИК, показывает вашь ник
-# при отказе бот вас посылает
 @bot.callback_query_handler(func=lambda call: True)
 def answer(call):
+    '''следующая функция показ айди и ника
+    он задаёт вопрос, на который вы можете ответить да, или нет
+    при согласии бот даёт выбор:
+    Показывает две кнопки, которые находятся внизу панели
+    конпка: МОЙ ID, показывает ваш ID, вауууу
+    кнопка: МОЙ НИК, показывает вашь ник
+    при отказе бот вас посылает'''
     if call.data == 'yes':
         markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
         item_id = types.KeyboardButton("МОЙ ID")
@@ -140,23 +121,43 @@ def answer(call):
                          reply_markup=markup_reply
                          )
     elif call.data == 'no':
-        bot.send_message(call.message.chat.id, "ну и иди далеко, и на долго")
+        markup_reply = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        itemi_id = types.KeyboardButton("Да")
+        itemi_username = types.KeyboardButton("нет")
+
+        markup_reply.add(itemi_id, itemi_username)
+        bot.send_message(call.message.chat.id, "вам так сложно?",
+                         reply_markup=markup_reply
+                         )
 
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
 
 
-# после вашего выбора, бот выдаёт результат
-# результатом будет то, что вы спросили
-# например, если вы спросили МОЙ ID, то вам покажут вашь айди
-# а если вы спросили МОЙ НИК, то вам выдастся ник
 @bot.message_handler(content_types=['text'])
 def get_text(message):
+    '''после вашего выбора, бот выдаёт результат
+результатом будет то, что вы спросили
+например, если вы спросили МОЙ ID, то вам покажут вашь айди
+а если вы спросили МОЙ НИК, то вам выдастся ник'''
     if message.text == "МОЙ ID":
         bot.send_message(message.chat.id, f"ваш айди: {message.from_user.id}")
     elif message.text == 'МОЙ НИК':
         bot.send_message(message.chat.id, f"ваш ник: {message.from_user.first_name} {message.from_user.last_name}")
 
 
+@bot.message_handler(content_types=['text'])
+def get_text2(message):
+    '''после вашего выбора, бот выдаёт результат
+результатом будет то, что вы спросили
+например, если вы спросили МОЙ ID, то вам покажут вашь айди
+а если вы спросили МОЙ НИК, то вам выдастся ник'''
+    if message.text == "Да":
+        bot.send_message(message.chat.id, "вы очень большой лентяй")
+    elif message.text == 'нет':
+        bot.send_message(message.chat.id, "вам не видно, но я смотрю на вас осуждаюше")
+
+
 bot.polling()
+#You_funny_history_bot
